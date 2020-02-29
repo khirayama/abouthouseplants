5783ad5a9f739c1a87dc6fa905b1bd80e6c36ab5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-02-29 21:35:21 +0900

    Update pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,18 @@ import Link from 'next/link'
 
 export const config = { amp: true }
 
-export default function HomePage() {
-  console.log('try');
-  const { data } = require('../init-data');
-  console.log(data);
-
+export default function HomePage(props: { message: string }) {
   return (
     <div>
       <h1>Index Page</h1>
-      <p>{data.message}</p>
+      <p>{props.message}</p>
       <Link href="/new">to New Page</Link>
       <amp-img src="/profile.png" width="300" height="300" />
     </div>
     );
+}
+
+HomePage.getInitialProps = () => {
+  const { data } = require('../init-data');
+  return data;
 }
--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -2,17 +2,18 @@ import Link from 'next/link'
 
 export const config = { amp: true }
 
-export default function NewPage() {
-  console.log('try');
-  const { data } = require('../init-data');
-  console.log(data);
-
+export default function NewPage(props: { message: string }) {
   return (
     <div>
       <h1>New Page</h1>
-      <p>{data.message}</p>
+      <p>{props.message}</p>
       <Link href="/">to Index Page</Link>
       <amp-img src="/profile.png" width="300" height="300" />
     </div>
     );
+}
+
+NewPage.getInitialProps = () => {
+  const { data } = require('../init-data');
+  return data;
 }
