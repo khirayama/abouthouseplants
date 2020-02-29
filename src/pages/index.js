8c3999730a51e6ce4518f5f3e85be0802a7c25b2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-02-29 20:18:14 +0900

    Create first page

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export const config = { amp: true }
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <amp-img src="/profile.png" width="300" height="300" />
+    </div>
+    );
+}
