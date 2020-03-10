abbb2b98dab5c07f95a7a02fbb73df38a0e905f0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-11 08:04:31 +0900

    Update Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
+import css from 'styled-jsx/css';
 import Head from 'next/head';
-
-import { ResetStyle } from '../styles/ResetStyle';
 
 type LayoutProps = {
   title: string;
@@ -10,17 +9,24 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
+const styles = css`
+  .container {
+    max-width: 768px;
+    margin: 0 auto;
+  }
+`;
+
 export function Layout(props: LayoutProps) {
   return (
     <div>
-      <ResetStyle />
+      <style jsx>{styles}</style>
       <Head>
         <meta charSet="utf-8" />
         <title>{props.title}</title>
         <meta name="description" content={props.description} />
         <meta name="keywords" content={props.keywords.join(',')} />
       </Head>
-      {props.children}
+      <div className="container">{props.children}</div>
     </div>
   );
 }
