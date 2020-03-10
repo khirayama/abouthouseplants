771b8b93ddde9c39e04079c7d81e434edd62fb4f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-11 08:06:52 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import css from 'styled-jsx/css';
 
+import { config as siteConfig } from '../config';
 import { resource, Resource, NoteResourceData } from '../utils/resource';
 import { Layout } from '../components/Layout';
 import { Header } from '../components/Header';
-import { Link } from '../components/Link';
+import { Footer } from '../components/Footer';
+import { FeaturedNote } from '../components/FeaturedNote';
 
 export const config = { amp: true };
 
@@ -11,33 +14,44 @@ type IndexPageProps = {
   pathname: string;
   resource: {
     intro: Resource<NoteResourceData>;
-    introLabels: Resource[];
+    labels: Resource[];
   };
 };
 
+const styles = css`
+  .featured-note-container {
+    text-align: center;
+    padding: 24px;
+  }
+`;
+
 export default function IndexPage(props: IndexPageProps) {
   const intro = props.resource.intro;
-  const introLabels = props.resource.introLabels.map(introLabel => introLabel.data.title);
+  const labels = props.resource.labels.map(label => label.data.title);
 
   return (
-    <Layout title={intro.data.title} description={intro.data.description} keywords={introLabels}>
-      <Header pathname={props.pathname} />
-      <Link to={intro.slug}>
-        <h3>{props.resource.intro.data.title}</h3>
-      </Link>
-    </Layout>
+    <>
+      <style jsx>{styles}</style>
+      <Layout title={siteConfig.name} description={intro.data.description} keywords={labels}>
+        <Header pathname={props.pathname} />
+        <div className="featured-note-container">
+          <FeaturedNote note={intro} />
+        </div>
+        <Footer />
+      </Layout>
+    </>
   );
 }
 
-IndexPage.getInitialProps = (props: any): IndexPageProps => {
+IndexPage.getInitialProps = (data: any): IndexPageProps => {
   const intro = resource.get<NoteResourceData>('notes', 'introduction');
-  const introLabels = resource.getCollection('labels', intro.data.labels);
+  const labels = resource.getCollection('labels', intro.data.labels);
 
   return {
-    pathname: props.pathname,
+    pathname: data.pathname,
     resource: {
       intro,
-      introLabels,
+      labels,
     },
   };
 };
