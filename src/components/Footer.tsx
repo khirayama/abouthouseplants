dd97c707e030026dbf600763d92549ec78e00be0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-12 09:28:56 +0900

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import css from 'styled-jsx/css';
 
 import { config } from '../config';
+import { Link } from '../components/Link';
 
 const styles = css`
   .footer {
@@ -10,6 +11,13 @@ const styles = css`
   }
   .copyright {
     font-size: 0.75rem;
+    color: #aaa;
+  }
+  .link-list {
+    font-size: 0.75rem;
+  }
+  .link-list-item {
+    display: inline-block;
   }
 `;
 
@@ -18,6 +26,11 @@ export function Footer() {
     <>
       <style jsx>{styles}</style>
       <footer className="footer">
+        <ul className="link-list">
+          <li className="link-list-item">
+            <Link to="/sitemap">サイトマップ</Link>
+          </li>
+        </ul>
         <small className="copyright">&copy; 2020 {config.name} All rights reserved.</small>
       </footer>
     </>
