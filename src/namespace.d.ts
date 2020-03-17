68333eb895e675a615ee34f6c251514b7b12221e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-18 08:25:21 +0900

    Add namespace

--- src/namespace.d.ts
+++ src/namespace.d.ts
@@ -1,5 +1,9 @@
 declare module 'remark-react';
 
+declare module 'remark-slug';
+
+declare module 'remark-autolink-headings';
+
 declare namespace JSX {
   interface IntrinsicElements {
     'amp-img': any;
