b586286bda17307bf9676587af453dbe1eb22bc8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-03-21 12:51:08 +0900

    Generate robots.txt

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -1,9 +1,14 @@
 import { generateSitemap, generateSitemapXML, saveSitemap } from './sitemap';
+import { generateRobotsTxt, saveRobotsTxt } from './robots';
 
 export function init() {
   console.log('--- Start Initilize---');
+  /* sitemap.xml */
   const sitemap = generateSitemap();
   const sitemapXML = generateSitemapXML(sitemap);
   saveSitemap(sitemapXML);
+  /* robots.txt */
+  const robotsTxt = generateRobotsTxt();
+  saveRobotsTxt(robotsTxt);
   console.log('--- End Initilize ---');
 }
